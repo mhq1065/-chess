21f884e5c6a861f4cd992114170ed184f857b82f
mhq1065 <mhq1065@qq.com>
2021-07-18 22:43:22 +0800

    修复了用户信息不保存的问题

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,12 +1,11 @@
 import ajax from "./ajax";
 import store from "@/store/index";
 // 注册
-export async function register(usernmae: string, pwd: string) {
-    const data = await ajax
-        .post("/register", {
-            username: usernmae,
-            password: pwd,
-        });
+export async function register(username: string, pwd: string) {
+    const data = await ajax.post("/register", {
+        username: username,
+        password: pwd,
+    });
     switch (data.data.state) {
         case 200:
             return data.data;
@@ -20,17 +19,20 @@ export async function register(usernmae: string, pwd: string) {
 }
 
 // 登录
-export async function login(usernmae: string, pwd: string) {
-    const data = await ajax
-        .post("/login", {
-            username: usernmae,
-            password: pwd,
-        });
+export async function login(username: string, pwd: string) {
+    const data = await ajax.post("/login", {
+        username: username,
+        password: pwd,
+    });
     switch (data.data.state) {
         // 注册成功
         case 200:
             // 保存token
             store.commit("updateJWT", data.data.token);
+            store.commit("updateUser", {
+                username: username,
+                pwd: pwd,
+            });
             return data.data;
         case 301: // 用户名不存在
             throw new Error("用户名不存在");
